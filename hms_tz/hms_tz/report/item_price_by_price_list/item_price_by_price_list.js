a2db0eb0dd9cc73f5876c8a2ea8c90bd825d83a2
iphenelist <innocentphenelist@gmail.com>
2026-02-11 10:19:09 +0300

    feat: enhance item price report with filters for price list and item price

--- hms_tz/hms_tz/report/item_price_by_price_list/item_price_by_price_list.js
+++ hms_tz/hms_tz/report/item_price_by_price_list/item_price_by_price_list.js
@@ -2,6 +2,19 @@
 // For license information, please see license.txt
 /* eslint-disable */
 
-frappe.query_reports["Item Price by Price List"] = {
-  filters: [],
+frappe.query_reports["Item Prices by Price List"] = {
+  filters: [
+    {
+      fieldname: "price_list",
+      label: __("Price List"),
+      fieldtype: "Link",
+      options: "Price List",
+    },
+    {
+      fieldname: "item_code",
+      label: __("Item Code"),
+      fieldtype: "Link",
+      options: "Item",
+    }
+  ],
 };
